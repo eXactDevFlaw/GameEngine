9c4d8a548e2ac9c8d37821cf79e839486179034c
eXactDevFlaw <lutzboelling@gmx.de>
2025-10-13 21:10:36 +0200

    added background config

--- src/game/root/gameObjects/maps/background.js
+++ src/game/root/gameObjects/maps/background.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import { assetBackgroundImagePostions } from "../../core/assetConfig.js";
 import {
     BackgroundLayer1,
     BackgroundLayer2,
@@ -9,12 +10,12 @@ import {
 } from "../../core/assetLoader.js";
 
 const BackgroundConf = [
-    { Path: BackgroundLayer1, ImageKey: "background-1", AnimKey: "background-1-anim", width: 524, height: 299},
-    { Path: BackgroundLayer2, ImageKey: "background-2", AnimKey: "background-2-anim", width: 524, height: 246},
-    { Path: BackgroundLayer3, ImageKey: "background-4", AnimKey: "background-4-anim", width: 499, height: 235},
-    { Path: BackgroundLayer4, ImageKey: "background-3", AnimKey: "background-3-anim", width: 475, height: 323},
-    { Path: BackgroundLayer5, ImageKey: "background-5", AnimKey: "background-5-anim", width: 520, height: 293},
-    { Path: BackgroundLayer6, ImageKey: "background-5", AnimKey: "background-5-anim", width: 520, height: 50}
+    { Path: BackgroundLayer1, ImageKey: "background-1", AnimKey: "background-1-anim",x: 0, y: 0, width: 524, height: 299},
+    { Path: BackgroundLayer2, ImageKey: "background-2", AnimKey: "background-2-anim",x: 0, y: 0, width: 524, height: 246},
+    { Path: BackgroundLayer3, ImageKey: "background-3", AnimKey: "background-3-anim",x: 0, y: 0, width: 499, height: 235},
+    { Path: BackgroundLayer4, ImageKey: "background-4", AnimKey: "background-4-anim",x: 0, y: 0, width: 475, height: 323},
+    { Path: BackgroundLayer5, ImageKey: "background-5", AnimKey: "background-5-anim",x: 0, y: 0, width: 520, height: 293},
+    { Path: BackgroundLayer6, ImageKey: "background-6", AnimKey: "background-6-anim",x: 800, y: 800, width: 520, height: 50}
 ]
 
 export default class ParallaxBackground {
@@ -32,15 +33,14 @@ export default class ParallaxBackground {
                 width: width, height: height,
             })
         })
-        ParallaxBackground.data = "Test"
     }
 
     createImage() {
-        BackgroundConf.forEach(({ImageKey, AnimKey}) => {
+        BackgroundConf.forEach(({ImageKey, AnimKey, x, y, width, height}) => {
             if (!this.scene.textures.exists(AnimKey)) {
-                this.bg = this.scene.add.image(0, 0, ImageKey)
+                this.bg = this.scene.add.image(x, y, ImageKey)
                 this.bg.setScale(4,4)
-                this.bg.setPosition(this.scene.scale.width * 0.5, this.scene.scale.height * 0.5)
+                // this.bg.setPosition(this.scene.scale.width * 0.5, this.scene.scale.height * 0.5)
                 this.bg.depth = -1000
             }
         })
